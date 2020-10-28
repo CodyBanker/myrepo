27767dfbd92b76debce4e8fa1017a8bd3a8d60e4
CodyBanker <52641269+CodyBanker@users.noreply.github.com>
2020-10-29 07:46:19 +0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-echo "hello world!"
+#!/bin/bash
+echo "Hello World"
+echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
+echo "AGENT_WORKFOLDER contents:"
+ls -1 $AGENT_WORKFOLDER
+echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
+echo "AGENT_BUILDDIRECTORY contents:"
+ls -1 $AGENT_BUILDDIRECTORY
+echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
+echo "Over and out."
