fd5152bdf449151838e808a4b962e49b8aac1e62
CodyBanker <52641269+CodyBanker@users.noreply.github.com>
2020-10-29 09:18:10 +0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,28 +1,5 @@
-#!/bin/bash
-
-appConfigName=myTestAppConfigStore
-#resource name must be lowercase
-myAppConfigStoreName=${appConfigName,,}
-myResourceGroupName=$appConfigName"Group"
-
-# Create resource group 
-az group create --name $myResourceGroupName --location eastus
-
-# Create the Azure AppConfig Service resource and query the hostName
-appConfigHostname=$(az appconfig create \
-  --name $myAppConfigStoreName \
-  --location eastus \
-  --resource-group $myResourceGroupName \
-  --query endpoint \
-  --sku free \
-  -o tsv
-  )
-
-# Get the AppConfig connection string 
-appConfigConnectionString=$(az appconfig credential list \
---resource-group $myResourceGroupName \
---name $myAppConfigStoreName \
---query "[?name=='Secondary Read Only'] .connectionString" -o tsv)
-
-# Echo the connection string for use in your application
-echo "$appConfigConnectionString"
+echo "Enter the Resource Group name:" &&
+read resourceGroupName &&
+echo "Enter the managed identity name:" &&
+read idName &&
+az identity show -g $resourceGroupName -n $idName --query id
