6f471283348b040e30eb517984a4376198e167d2
CodyBanker <52641269+CodyBanker@users.noreply.github.com>
2020-10-29 08:40:02 +0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-echo "Hello World"
-echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
-echo "AGENT_WORKFOLDER contents:"
-ls -1 $AGENT_WORKFOLDER
-echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
-echo "AGENT_BUILDDIRECTORY contents:"
-ls -1 $AGENT_BUILDDIRECTORY
-echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
-echo "Over and out."
+
+appConfigName=myTestAppConfigStore
+#resource name must be lowercase
+myAppConfigStoreName=${appConfigName,,}
+myResourceGroupName=$appConfigName"Group"
+
+# Create resource group 
+az group create --name $myResourceGroupName --location eastus
+
+# Create the Azure AppConfig Service resource and query the hostName
+appConfigHostname=$(az appconfig create \
+  --name $myAppConfigStoreName \
+  --location eastus \
+  --resource-group $myResourceGroupName \
+  --query endpoint \
+  --sku free \
+  -o tsv
+  )
+
+# Get the AppConfig connection string 
+appConfigConnectionString=$(az appconfig credential list \
+--resource-group $myResourceGroupName \
+--name $myAppConfigStoreName \
+--query "[?name=='Secondary Read Only'] .connectionString" -o tsv)
+
+# Echo the connection string for use in your application
+echo "$appConfigConnectionString"
